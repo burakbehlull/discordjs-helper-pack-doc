a87d4b5e0d1b443cb0ae085c26f1a79d8f0dac3e
burakbehlull <burakyabgu@gmail.com>
2024-11-04 22:40:31 +0300

    fix: Deploy issues

---
+++ src/pages/getting-started/meta.tsx
@@ -0,0 +1,15 @@
+// pages/getting-started/meta.tsx
+
+import React from 'react';
+
+const Meta = () => null;
+
+export const meta = {
+  title: 'Getting Started',
+  children: {
+    'what-is-package': 'What is Package',
+    'quick-start': 'Quick Start',
+  },
+};
+
+export default Meta;
