c6da3bcbd39f4396ebff7b8d16a82a1684bf31ac
burakbehlull <burakyabgu@gmail.com>
2024-11-05 15:07:11 +0300

    feat: Style home page build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-
+import '../app/globals.css'
 export default function index(){
 
 	return (
@@ -7,17 +7,23 @@ export default function index(){
 		<>
 			<section id='home'>
 				<header>
+					
 					<nav>
 						<ul>
-							<li><Link href="/">Home</Link></li>
+							<li className="btn1"><Link href="/">Home</Link></li>
+							<li className="btn1"><Link href="https://www.npmjs.com/package/discordjs-helper-pack">Package Visit</Link></li>
 						</ul>
 					</nav>
 				</header>
 				<main>
-					<h1>Discordjs Helper Pack</h1>
+				<h1 id="title">Discordjs Helper Pack</h1>
 					<div>
+						
 						<ul>
-							<li><Link href='/getting-started/quick-start'>Getting Started</Link></li>
+							
+							<li className="btn2"><Link href='/getting-started/quick-start'>Getting Started</Link></li>
+							
+						
 						</ul>
 					</div>
 				</main>
