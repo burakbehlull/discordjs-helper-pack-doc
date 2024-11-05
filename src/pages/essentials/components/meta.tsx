5925638b24bee63ece63b7babf7368623dc0a866
burakbehlull <burakyabgu@gmail.com>
2024-11-05 13:34:13 +0300

    feat: Add new documents

---
+++ src/pages/essentials/components/meta.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Meta = () => null
+
+export const meta = {
+  title: 'Components',
+  children: {
+    'textselectbox': 'textselectbox',
+	'userselectbox': 'userselectbox'
+  },
+}
+
+export default Meta
---
+++ src/pages/essentials/meta.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Meta = () => null
+
+export const meta = {
+  title: 'Essentials',
+  children: {
+    'configuration': 'configuration',
+	'components': 'components'
+  },
+}
+
+export default Meta
